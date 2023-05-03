6f2b037cba313c5b7e8a001eb2d4a08bc9c3e0a5
Alex Qin <qianjin.qin@qq.com>
2023-05-03 10:11:08 +0000

    update master branch for the Netlify deployment

---
+++ _astro/jsx-runtime.2f0084d5.js
@@ -0,0 +1,9 @@
+import{r as i}from"./index.03be2d59.js";const a={English:"en"};Object.values(a);const d={indexName:"XXXXXXXXXX",appId:"XXXXXXXXXX",apiKey:"XXXXXXXXXX"};var _={exports:{}},X={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var l=i,u=Symbol.for("react.element"),m=Symbol.for("react.fragment"),x=Object.prototype.hasOwnProperty,y=l.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function f(t,e,s){var r,o={},n=null,p=null;s!==void 0&&(n=""+s),e.key!==void 0&&(n=""+e.key),e.ref!==void 0&&(p=e.ref);for(r in e)x.call(e,r)&&!c.hasOwnProperty(r)&&(o[r]=e[r]);if(t&&t.defaultProps)for(r in e=t.defaultProps,e)o[r]===void 0&&(o[r]=e[r]);return{$$typeof:u,type:t,key:n,ref:p,props:o,_owner:y.current}}X.Fragment=m;X.jsx=f;X.jsxs=f;_.exports=X;var v=_.exports;export{d as A,a as K,v as j};
