3b9a2c32bb6b369891fa5105a36df9c37c600660
Alex Qin <qianjin.qin@qq.com>
2024-12-30 14:32:39 +0000

    update master branch for the Netlify deployment

---
+++ _astro/jsx-runtime.BoOPUaNh.js
@@ -0,0 +1,9 @@
+import{r as l}from"./index.BljroyU7.js";const E={English:"en"},N={indexName:"XXXXXXXXXX",appId:"XXXXXXXXXX",apiKey:"XXXXXXXXXX"};var s={exports:{}},t={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var _;function y(){if(_)return t;_=1;var f=l(),m=Symbol.for("react.element"),R=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,x=f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,d={key:!0,ref:!0,__self:!0,__source:!0};function u(n,e,X){var r,o={},i=null,p=null;X!==void 0&&(i=""+X),e.key!==void 0&&(i=""+e.key),e.ref!==void 0&&(p=e.ref);for(r in e)c.call(e,r)&&!d.hasOwnProperty(r)&&(o[r]=e[r]);if(n&&n.defaultProps)for(r in e=n.defaultProps,e)o[r]===void 0&&(o[r]=e[r]);return{$$typeof:m,type:n,key:i,ref:p,props:o,_owner:x.current}}return t.Fragment=R,t.jsx=u,t.jsxs=u,t}var a;function v(){return a||(a=1,s.exports=y()),s.exports}var h=v();export{N as A,E as K,h as j};
--- _astro/jsx-runtime.f72bd3ef.js
+++
@@ -1,9 +0,0 @@
-import{r as i}from"./index.03be2d59.js";const d={English:"en"},E={indexName:"XXXXXXXXXX",appId:"XXXXXXXXXX",apiKey:"XXXXXXXXXX"};var _={exports:{}},X={};/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var a=i,l=Symbol.for("react.element"),m=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,x=a.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,y={key:!0,ref:!0,__self:!0,__source:!0};function f(t,r,s){var e,o={},n=null,p=null;s!==void 0&&(n=""+s),r.key!==void 0&&(n=""+r.key),r.ref!==void 0&&(p=r.ref);for(e in r)u.call(r,e)&&!y.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:l,type:t,key:n,ref:p,props:o,_owner:x.current}}X.Fragment=m;X.jsx=f;X.jsxs=f;_.exports=X;var O=_.exports;export{E as A,d as K,O as j};
